14174a0316893795656cfbe84e77600c6b190a1a
Alejandro Landaverde <adlandav@ucsc.edu>
2020-01-21 17:26:33 -0800

    initialized project

---
+++ Assets/script/menus/MainMenue.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MainMenue : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject findOpponnentPanel = null;
+    [SerializeField] private GameObject waitingStatusPanel = null;
+    [SerializeField] private TextMeshProUGUI waitingStatusText = null;
+
+    private bool isConnecting = false;
+    private const string GameVersion = "0.1"; //Change with the gameVersion
+    private const int MaxPlayersPerRoom = 2;
+
+    private void Awake() => PhotonNetwork.AutomaticallySyncScene = true;
+
+    public void FindOpponent() {
+        isConnecting = true;
+
+        findOpponnentPanel.SetActive(false);
+        waitingStatusPanel.SetActive(true);
+
+        waitingStatusText.text = "searching...";
+
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.GameVersion = GameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("connected to master");
+        if (isConnecting) {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        waitingStatusPanel.SetActive(false);
+        findOpponnentPanel.SetActive(true);
+
+        Debug.Log($"Disconnected due to {cause}");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("No clients waiting, creating new room");
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = MaxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("client joined the room succesfully");
+
+        int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+
+        if (playerCount != MaxPlayersPerRoom)
+        {
+            waitingStatusText.text = "waiting for opponent";
+
+            Debug.Log("client is waiting for opponent");
+        }
+        else {
+            waitingStatusText.text = "opponent found";
+            Debug.Log("match is ready to begin");
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayersPerRoom) {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+
+            waitingStatusText.text = "opponent found";
+            Debug.Log("Match is ready to begin");
+
+            PhotonNetwork.LoadLevel("testScene");
+        }
+    }
+}
---
+++ Assets/script/menus/PlayerNameInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+using Photon.Pun;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameInputField = null;
+    [SerializeField] private Button continueButton = null;
+
+    private const string PlayerPrefsNameKey = "PlayerName";
+
+    private void Start() => SetUPInputField();
+
+    private void SetUPInputField()
+    {
+        if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) { return; }
+
+        string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey);
+
+        nameInputField.text = defaultName;
+
+        SetPlayername(defaultName);
+    }
+
+    public void SetPlayername(string name)
+    {
+        //For some reason using just the player name doesn't work
+
+        //Debug.Log(nameInputField.text);
+        continueButton.interactable = (nameInputField.text.Length > 0) ? true : false;
+        //continueButton.interactable = !string.IsNullOrEmpty(name);
+    }
+
+    public void SavePlayerName() {
+        string playerName = nameInputField.text;
+
+        PhotonNetwork.NickName = playerName;
+
+        PlayerPrefs.SetString(PlayerPrefsNameKey, playerName);
+    }
+}
